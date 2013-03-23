c879c8f8297785609fd87255b1a600aea31572a1
mediatronix@gmail.com <mediatronix@gmail.com@e6ff1368-d33c-4ed3-1708-30a8928a3129>
2013-03-23 09:10:05 +0000

    cheby and kaiser separated estimators added

--- trunk/windowCheby.h
+++ trunk/windowCheby.h
@@ -1,6 +1,6 @@
 #ifndef WINDOWCHEBY_H
 #define WINDOWCHEBY_H
 
-void chebyshev( long double * W, int aLen, double aAtten ) ;
+void chebyshev( long double * W, int aLen, double beta ) ;
 
 #endif // WINDOWCHEBY_H
--- trunk/windowKaiser.cpp
+++ trunk/windowKaiser.cpp
@@ -13,22 +13,13 @@ static long double I0( long double x ) {
     return s ;
 }
 
-void kaiser( long double * W, int aLen, double aAtten ) {
-    long double alpha ;
-
-    if ( aAtten < 21.0 )
-        alpha = 0.0L ;
-    else if ( aAtten > 50.0 )
-        alpha = 0.1102L *( aAtten- 8.7 ) ;
-    else
-        alpha = 0.5842L * pow( ( aAtten - 21.0 ), 0.4 ) + 0.07886 * ( aAtten - 21 ) ;
-
-    long double den = I0( M_PI * alpha ) ;
+void kaiser( long double * W, int aLen, double beta ) {
+    long double den = I0( M_PI * beta ) ;
     int nOn2 = aLen / 2 ;
 
     W[ nOn2 ] = 1 ;
     for ( int j = 1 ; j < nOn2 ; j += 1 ) {
-        double t = I0( (long double)M_PI * alpha * sqrt( 1.0L - (long double)(j * j) / (long double)( nOn2 * nOn2 ) ) ) ;
+        double t = I0( (long double)M_PI * beta * sqrt( 1.0L - (long double)(j * j) / (long double)( nOn2 * nOn2 ) ) ) ;
         W[ nOn2 + j ] = W[ nOn2 - j ] = 1 * t / den ;
     }
 }
